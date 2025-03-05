153438093dd2df0d917c2609e7545f84d8d4788d
Alan <albou@netc.fr>
2025-03-05 22:58:41 +0100

    Initial Commit

---
+++ TapTic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TapTic
+//
+//  Created by Boubou on 05/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapTic/TapTicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTicApp.swift
+//  TapTic
+//
+//  Created by Boubou on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TapTicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
